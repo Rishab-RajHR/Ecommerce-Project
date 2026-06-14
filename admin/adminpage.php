1310def1c56a82340ad4384834dd3055e11d6e32
Rishab Raj <rr0948555@gmail.com>
2026-06-14 08:23:08 +0530

    Admin CSS

--- admin/adminpage.php
+++ admin/adminpage.php
@@ -18,6 +18,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Admin Page</title>
+  <link rel="stylesheet" href="admin_style.css">
 </head>
 <body>
     
